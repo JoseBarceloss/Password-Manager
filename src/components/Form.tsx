273087a2429286bd9f1f8e970404f62636254c76
Barcelos <barcelosps41710@gmail.com>
2023-08-16 00:37:35 -0300

    exercicio 2 completo

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,1 +1,22 @@
-z
+function Form() {
+  return (
+    <div className="form-container">
+      <label htmlFor="nome-servico">Nome do serviço:</label>
+      <input type="text" id="nome-servico" />
+
+      <label htmlFor="login">Login:</label>
+      <input type="text" id="login" />
+
+      <label htmlFor="senha">Senha:</label>
+      <input type="password" id="senha" />
+
+      <label htmlFor="url">URL:</label>
+      <input type="text" id="url" />
+
+      <button>Cadastrar</button>
+      <button>Cancelar</button>
+    </div>
+  );
+}
+
+export default Form;
